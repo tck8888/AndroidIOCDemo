3e9ac2127d6083da576d47444cf91ce25928c764
tck <tck@dra100.com>
2020-01-21 17:48:10 +0800

    调用编写好的注解组件MyButterKnifeTrain

--- app/src/main/java/com/tck/androidiocdemo/MainActivity.java
+++ app/src/main/java/com/tck/androidiocdemo/MainActivity.java
@@ -3,12 +3,26 @@ package com.tck.androidiocdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.tck.libannotation.butterknife.BindView;
+import com.tck.libannotation.butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_article)
+    private ImageButton ibArticle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.btn_article)
+    public void onClick(View view) {
+
     }
 }
