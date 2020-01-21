f9de106a03d9f7e1b67c52148bee902a05b2d0b2
tck <tck@dra100.com>
2020-01-21 18:25:51 +0800

    编写调试注解

--- app/src/main/java/com/tck/androidiocdemo/MainActivity.java
+++ app/src/main/java/com/tck/androidiocdemo/MainActivity.java
@@ -4,25 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.tck.libannotation.butterknife.BindView;
 import com.tck.libannotation.butterknife.OnClick;
+import com.tck.mybutterknife.MyButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_article)
-    private ImageButton ibArticle;
+    public Button ibArticle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyButterKnife.bind(this);
     }
 
     @OnClick(R.id.btn_article)
     public void onClick(View view) {
-
+        Toast.makeText(this, "文章", Toast.LENGTH_SHORT).show();
     }
 }
--- mybutterknife/src/main/java/com/tck/mybutterknife/DebouncingOnClickListener.java
+++ mybutterknife/src/main/java/com/tck/mybutterknife/DebouncingOnClickListener.java
@@ -1,13 +1,15 @@
 package com.tck.mybutterknife;
 
+
 import android.view.View;
 
 public abstract class DebouncingOnClickListener implements View.OnClickListener {
     @Override
     public void onClick(View v) {
-
+        // 调用抽象方法
         doClick(v);
     }
 
-    abstract void doClick(View view);
+    // 抽象方法，子类new必须实现该方法
+    protected abstract void doClick(View v);
 }
